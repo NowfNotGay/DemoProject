2cef0fba3ba76f544e0709fccfdf20ac0b5d189c
NowfNotGay <108786056+NowfNotGay@users.noreply.github.com>
2023-03-19 15:19:57 +0700

    Add project files.

---
+++ DemoProject_MVC/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoProject_MVC.Areas.Admin.Controllers;
+[Area("Admin")]
+[Route("admin/dashboard")]
+public class DashboardController : Controller
+{
+    [Route("index")]
+    [Route("")]
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ DemoProject_MVC/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoProject_MVC.Controllers;
+[Route("home")]
+public class HomeController : Controller
+{
+    [Route("~/")]
+    [Route("")]
+    [Route("index")]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
